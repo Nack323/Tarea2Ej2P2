504618e68d7592f6a0a44af9ebae2cf7730c8ee3
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 17:59:24 -0600

    Create UserManager.cpp

---
+++ UserManager.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "UserManager.hpp"
+
+UserManager::UserManager(FacultyManager* facultyManager, StudentManager* studentManager){
+
+};
+
+User* UserManager::validateCredentials(std::string userName, std::string contrasenia){
+
+};
+
+void UserManager::editUser(User* usuario){
+
+};
+
+void UserManager::showUser(User* usuario){
+
+};
+
+std::string UserManager::getNewBannerID(){
+
+};
