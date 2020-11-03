d80e9e6c620e2702a71a989cfe0d7c823dcef2ef
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 18:16:02 -0600

    Create StudentManager.cpp

---
+++ StudentManager.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include "StudentManager.hpp"
+
+void StudentManager::loadUnderGrads(){
+
+};
+
+void StudentManager::updateUnderGrads(){
+
+};
+
+StudentManager::StudentManager(char path[]:){
+
+};
+
+void StudentManager::createNewStudent(UserManager* um){
+
+};
+
+Student* StudentManager::getStudentByID(std::string BannerID){
+
+};
+
+Student* StudentManager::getAllStudents(){
+
+};
+
+void StudentManager::editStudent(){
+
+};
+
+void StudentManager::showStudents(){
+
+};
+
+void StudentManager::deleteStudent(){
+
+};
