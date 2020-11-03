4d2f6238b6a706139b8284df9f71e7964efb5cbc
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 19:13:10 -0600

    Create CourseManager.cpp

---
+++ CourseManager.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "CourseManager.cpp"
+
+void CourseManager::loadCourses(){
+
+};
+
+void CourseManager::updateCourses(){
+
+};
+
+CourseManager::CourseManager(char path[]:, StudentManager* sm, FacultyManager* fm){
+
+};
+
+void CourseManager::createNewCourse(){
+
+};
+
+void CourseManager::editCourse(){
+
+};
+
+void CourseManager::showCourses(){
+
+};
+
+void CourseManager::showClassByID(std::string nrc){
+
+};
+
+void CourseManager::deleteCourse(){
+
+};
