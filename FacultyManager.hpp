4e4ab6939434145e1bda3bacaada030045f0d2ba
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 18:28:17 -0600

    Create FacultyManager.hpp

---
+++ FacultyManager.hpp
@@ -0,0 +1,20 @@
+#ifndef FacultyManager_hpp
+#define FacultyManager_hpp
+
+class FacultyManager{
+private:
+    Faculty* profesores;
+    char pathFaculty[]:;
+    void loadFaculty();
+    void updateFaculty();
+public:
+    FacultyManager(char);
+    void createNewFaculty(UserManager*);
+    Faculty* getFacultyByID(std::string);
+    Faculty* getAllFaculty();
+    void editFaculty();
+    void showFaculty();
+    void deleteFaculty();
+};
+
+#endif /* FacultyManager_hpp */
