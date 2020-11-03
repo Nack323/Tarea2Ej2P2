8da9536104e293e37d6c3591c73274d793619b7a
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 18:07:08 -0600

    Create StudentManager.hpp

---
+++ StudentManager.hpp
@@ -0,0 +1,20 @@
+#ifndef StudentManager_hpp
+#define StudentManager_hpp
+
+class StudentManager{
+private:
+    Student* estudiantes;
+    char pathUndergraduate[];
+    void loadUnderGrads();
+    void updateUnderGrads();
+public:
+    StudentManager(char);
+    void createNewStudent(UserManager*);
+    Student* getStudentByID(std::string);
+    Student* getAllStudents();
+    void editStudent();
+    void showStudents();
+    void deleteStudent();
+};
+
+#endif /* StudentManager_hpp */
