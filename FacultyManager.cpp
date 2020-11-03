64862fa4d85fc4640369a156c30301cc3b21d898
herondaleFray <73211792+herondaleFray@users.noreply.github.com>
2020-11-02 18:59:33 -0600

    Create FacultyManager.cpp

---
+++ FacultyManager.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "FacultyManager.hpp"
+
+void FacultyManager::loadFaculty(){
+
+};
+
+void FacultyManager::updateFaculty(){
+
+};
+
+FacultyManager::FacultyManager(char path[]:){
+
+};
+
+void FacultyManager::createNewFaculty(UserManager* um){
+
+};
+
+Faculty* FacultyManager::getFacultyByID(std::string BannerID){
+
+};
+
+Faculty* FacultyManager::getAllFaculty(){
+
+};
+
+void FacultyManager::editFaculty(){
+
+};
+
+void FacultyManager::showFaculty(){
+
+};
+
+void FacultyManager::deleteFaculty(){
+
+};
